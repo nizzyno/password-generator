39be321e6ecc0674f3be48e5da74876b84b5038b
Noah <mcbeasty11.nh@icloud.com>
2021-03-06 22:31:55 -0600

    script variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
+
 // Assignment code here
-
-
+// User input variables:
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+// Password variable values:
+// Special characters
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
+  "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@",
+  "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+// Numeric characters
+number = [1,2, 3, 4, 5, 6, 7, 8, 9];
+// Alphabetical characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Space is for the Uppercase conversion
+space = [];
+// Choices declared outside the if statement so they can be concatenated upon condition
+var choices;
+// converts letters to uppercase 
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+// creates a variable for uppercase conversion
+alpha2 = alpha.map(toUpper);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
